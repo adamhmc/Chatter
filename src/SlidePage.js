b60d3a58c4558024478b33c5c543146db608e9e9
Adamhmc <adamhmc@gmail.com>
2016-04-28 21:49:26 -0700

    workable app

--- src/SlidePage.js
+++ src/SlidePage.js
@@ -19,28 +19,23 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#9DD6EB',
   },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5',
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9',
-  },
+
   text: {
     color: '#fff',
     fontSize: 30,
     fontWeight: 'bold',
   },
   photo: {
-  	width:60,
-  	height:60
+  	width:200,
+  	height:200
   }
 })
+
+var teamVoyager = [
+{"Category":"Celebrity", "UserName": "Brian Luc", "UserId":"005B0000000EO4DIAW"},
+{"Category":"ChatterBox", "UserName": "Brian Luc", "UserId":"005B0000000EO4DIAW"}, 
+{"Category":"NeedsTherapy", "UserName": "Rob Storrs", "UserId":"005B0000000GySKIA0"},
+{"Category":"Works For Home", "UserName": "Terry Martin", "UserId":"005B0000000Ec00IAC"}];
 
 class SwiperPage extends Component{
   constructor(props) {
@@ -50,23 +45,34 @@ class SwiperPage extends Component{
         };
     }
 
+
+  getRandomStyle() {
+      var backgroundColorArray = ["#92BBD9","#EBD4C1","#EBC1D1","#C4ECCC","#E2C1EB","#97CAE5","#9DD6EB","#364551","#92D9AF","#F1E1D5","#5DBD3D"];
+      var colorIndex = Math.floor((Math.random()*backgroundColorArray.length));
+    return backgroundColorArray[colorIndex];
+  }
+
 	render() {
-    console.log(this.state.teamName);
-    return (
+      var rows = [];
+      for (var i=0; i < teamVoyager.length; i++) {
+        
+        var photoSrc = './photo/'+teamVoyager[i]["UserId"]+'.jpg';
+        console.log(photoSrc.toString());
+
+        var bgColor = this.getRandomStyle();
+        rows.push(
+          <View style={[ styles.slide1, {backgroundColor:bgColor}]} key={i}>
+          <Text>{teamVoyager[i]["Category"]}</Text>
+
+          <Image source={require('./photo/005B0000000GySKIA0.jpg')} style={styles.photo}/>
+          <Text>{teamVoyager[i]["UserName"]}</Text>
+          </View>
+        );
+      }    
+      return (
     	<View>
       <Swiper style={styles.wrapper} showsButtons={false}>
-        <View style={styles.slide1}>
-                      <Image
-        style={styles.photo}
-        source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}/>
-          <Text style={styles.text}>Hello Swiper</Text>
-        </View>
-        <View style={styles.slide2}>
-          <Text style={styles.text}>Beautiful</Text>
-        </View>
-        <View style={styles.slide3}>
-          <Text style={styles.text}>And simple</Text>
-        </View>
+        {rows}
       </Swiper>
       </View>
     )
